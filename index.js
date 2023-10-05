bf742fa3bfaf51dc79a1357afd211cee7dc1c303
Abu-safyan yusuf <abusufyanyusuf080@gmail.com>
2023-10-05 21:09:23 +0100

    readme update

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ const sr = ScrollReveal({
 })
 
 sr.reveal(`.image--home, .aboutText, .card-right`, {origin: 'right'})
-sr.reveal(` .about, .card--left `, {origin: 'left'})
+srreveal(` .about, .card--left `, {origin: 'left'})
  sr.reveal(`.text-top, .latest--title, .card--top, .nav`, {origin: 'top'})
  sr.reveal(`.text-bottom, .card--bottom`, {origin: 'bottom'})
